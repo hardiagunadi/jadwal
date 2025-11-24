190738358a768faba05797968b207220dd28d8e4
Hardi Agunadi <zigunx01@gmail.com>
2025-11-24 15:06:14 +0700

    Ci/CD

--- resources/views/public/agenda/tv.blade.php
+++ resources/views/public/agenda/tv.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Agenda Hari Ini - Layar TV</title>
+    <title>Agenda Kecamatan Watumalang Hari Ini - Layar TV</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://cdn.tailwindcss.com"></script>
