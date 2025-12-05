b593dc2f29dc1f7beaae5dc1ce52bd00091f53f2
Hardi Agunadi <zigunx01@gmail.com>
2025-12-05 18:42:38 +0700

    fix app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\KirimPengingatTindakLanjut;
+use App\Console\Commands\RemindTindakLanjutCommand;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withCommands([
         KirimPengingatTindakLanjut::class,
+        RemindTindakLanjutCommand::class,
     ])
     ->withSchedule(function (Schedule $schedule) {
         // Jalankan scheduler pengingat TL setiap menit agar window 5 jam dan pengingat akhir tidak terlewat.
