00be798b2232dc85dee074b0240d1a7dd44efa73
Hardi Agunadi <zigunx01@gmail.com>
2025-12-11 14:16:14 +0700

    add  grup ID

---
+++ app/Filament/Resources/Groups/Schemas/GroupForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Groups\Schemas;
+
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class GroupForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Data Grup')
+                    ->schema([
+                        TextInput::make('nama')
+                            ->label('Nama Grup')
+                            ->required()
+                            ->maxLength(255),
+
+                        TextInput::make('wablas_group_id')
+                            ->label('ID Grup Wablas')
+                            ->helperText('Isi dengan ID grup dari Wablas (1203xxxxxxxxxx).')
+                            ->maxLength(255),
+
+                        Textarea::make('keterangan')
+                            ->label('Keterangan')
+                            ->rows(3),
+                    ])
+                    ->columns(1),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Groups/Tables/GroupsTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Resources\Groups\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class GroupsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama')
+                    ->label('Nama Grup')
+                    ->searchable()
+                    ->sortable()
+                    ->wrap(),
+
+                TextColumn::make('wablas_group_id')
+                    ->label('ID Grup Wablas')
+                    ->searchable()
+                    ->copyable(),
+
+                TextColumn::make('keterangan')
+                    ->label('Keterangan')
+                    ->limit(50)
+                    ->tooltip(fn ($state) => $state),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
