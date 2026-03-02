71b9bdd4271e18e97476a27ae68d6b7a9fa96d49
Hardi Agunadi <hardiagunadi1@gmail.com>
2026-03-02 08:12:00 +0700

    fix error login

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -2,13 +2,18 @@
 
 namespace App\Filament\Auth;
 
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Schemas\Components\Component;
 use Filament\Schemas\Components\View;
 use Filament\Auth\Http\Responses\Contracts\LoginResponse;
 use Filament\Schemas\Schema;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\ValidationException;
 use SensitiveParameter;
@@ -105,6 +110,14 @@ class Login extends BaseLogin
 
     public function authenticate(): ?LoginResponse
     {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            $this->getRateLimitedNotification($exception)?->send();
+
+            return null;
+        }
+
         if ($this->isRecaptchaEnabled()) {
             $token = $this->data['g_recaptcha_response'] ?? '';
 
@@ -127,6 +140,34 @@ class Login extends BaseLogin
             }
         }
 
-        return parent::authenticate();
+        $data = $this->form->getState();
+
+        /** @var SessionGuard $authGuard */
+        $authGuard = Filament::auth();
+
+        $authProvider = $authGuard->getProvider(); /** @phpstan-ignore-line */
+        $credentials = $this->getCredentialsFromFormData($data);
+
+        $user = $authProvider->retrieveByCredentials($credentials);
+
+        if ((! $user) || (! $authProvider->validateCredentials($user, $credentials))) {
+            $this->fireFailedEvent($authGuard, $user, $credentials);
+            $this->throwFailureValidationException();
+        }
+
+        if (! $authGuard->attemptWhen($credentials, function (Authenticatable $user): bool {
+            if (! ($user instanceof FilamentUser)) {
+                return true;
+            }
+
+            return $user->canAccessPanel(Filament::getCurrentOrDefaultPanel());
+        }, $data['remember'] ?? false)) {
+            $this->fireFailedEvent($authGuard, $user, $credentials);
+            $this->throwFailureValidationException();
+        }
+
+        session()->regenerate();
+
+        return app(LoginResponse::class);
     }
 }
